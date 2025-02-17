3635b105683954a620fe69bd99fd39a16e203c19
jrunzer26 <jrunzer26@hotmail.com>
2025-02-17 14:13:06 -0500

    Update installation script to use current directory for target path

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ winget install GlazeWM
 
 # Define the source and target paths
 $sourcePath = "C:\Users\$Env:USERNAME\.glzr\glazewm\config.yaml"
-$targetPath = "C:\source\glazewm\config.yaml"
+$targetPath = "$pwd\config.yaml"
 
 echo "Creating a symbolic link from $sourcePath to $targetPath"
 cmd /c mklink $sourcePath $targetPath
