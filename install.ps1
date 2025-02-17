1a745e39abad6f10f5f48156c52215ff666911f2
jrunzer26 <jrunzer26@hotmail.com>
2025-02-17 14:06:48 -0500

    Add initial configuration and installation script for GlazeWM

---
+++ install.ps1
@@ -0,0 +1,11 @@
+echo "Run this in an administrator powershell terminal"
+
+echo "Installing GlazeWM"
+winget install GlazeWM
+
+# Define the source and target paths
+$sourcePath = "C:\Users\$Env:USERNAME\.glzr\glazewm\config.yaml"
+$targetPath = "C:\source\glazewm\config.yaml"
+
+echo "Creating a symbolic link from $sourcePath to $targetPath"
+cmd /c mklink $sourcePath $targetPath
